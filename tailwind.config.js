166dd3f587df7ff2a597ae9f68e700095a3df5d2
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-16 18:12:10 +0700

    install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,18 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     "./storage/framework/views/*.php",
     "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
