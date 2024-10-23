a89b86a3b639d7aca048a0a3328e5340cfd155d2
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-23 15:19:51 +0700

    Refactor User model and migration to include role_id and phone_number fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,36 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['role_id', 'name', 'email', 'password', 'phone_number'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password'];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function scopeIsAdmin(): bool
+    {
+        return $this->role->name === 'admin';
+    }
+
+    public function scopeIsWorker(): bool
+    {
+        return $this->role->name === 'worker';
+    }
+
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
