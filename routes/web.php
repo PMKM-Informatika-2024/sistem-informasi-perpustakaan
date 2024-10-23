8ba55f1e02bec43992cde31894239cc39bfb8208
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-23 15:28:32 +0700

    Refactor routes to include login, registration, and dashboard functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Response::redirectTo('/login');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index']);
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+    Route::get('/register', [RegisterController::class, 'index']);
+    Route::post('/register', [RegisterController::class, 'register'])->name('register');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard.dashboard', [
+            'title' => 'Dashboard - Manajemen Perpustakaan',
+        ]);
+    });
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
