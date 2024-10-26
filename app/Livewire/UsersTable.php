f17bda6761230b58cf270de1e37f311c207dc1db
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-27 02:30:39 +0700

    livewire component and view

---
+++ app/Livewire/UsersTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Livewire\WithPagination;
+use Livewire\Attributes\Url;
+use App\Models\Role;
+use App\Models\User;
+
+class UsersTable extends Component
+{
+    use WithPagination;
+
+    #[Url(as: "search", history: true)]
+    public $keyword = '';
+
+    private function search(string $keyword)
+    {
+        return User::where("name", "LIKE", "%{$keyword}%")->whereRelation("role", "name", "!=", "admin")->paginate(6)->onEachSide(1)->withQueryString();
+    }
+
+    private function allExceptAdmin()
+    {
+        if (request()->has("role")) {
+            return User::whereRelation("role", "name", "!=", "admin")->whereRelation("role", "name", request("role"))->paginate(6)->onEachSide(1)->withQueryString();
+        }
+
+        return User::whereRelation("role", "name", "!=", "admin")->paginate(6)->onEachSide(1)->withQueryString();
+    }
+
+    public function render()
+    {
+        return view('livewire.users-table')->with([
+            "roles" => Role::all()->reject(function ($role) {
+                return $role->name === "admin";
+            }),
+            "users" => $this->keyword === '' ? $this->allExceptAdmin() : $this->search($this->keyword),
+        ]);
+    }
+
+    public function paginationView()
+    {
+        return 'vendor.pagination.tailwind';
+    }
+}
