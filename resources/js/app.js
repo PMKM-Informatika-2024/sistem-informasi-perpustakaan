c87e6bb3923a034c03bb77fc01fd4ebd08b68e23
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-27 02:28:38 +0700

    js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 import "flowbite";
+import { Dismiss } from "flowbite";
+
+const progress = document.getElementById("progress");
+
+const toast = new Dismiss(document.getElementById("toast-success"), null, {
+  timing: "ease-out",
+  transition: "transition-all",
+});
+
+setTimeout(() => {
+  toast.hide();
+}, 3000);
+
+(function () {
+  let step = 0;
+
+  if (step == 0) {
+    step = 1;
+
+    let width = 1;
+    const interval = setInterval(() => {
+      if (width >= 100) {
+        clearInterval(interval);
+        step = 0;
+      } else {
+        width++;
+        progress.style.width = `${width}%`;
+      }
+    }, 30);
+  }
+})();
