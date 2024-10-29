62b9dd9f884dc6c8ef8e8f7763ff0502c16dd0d1
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-30 00:58:58 +0700

    Refactor UserController to improve user management and deletion functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,37 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AddMemberRequest;
 use App\Models\User;
 use App\Models\Role;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class UserController
 {
-    public function index(Request $request)
+    public function index()
     {
         return view("dashboard.manage.user", [
-            "title" => "Manage Member",
+            "title" => "Manage User",
             "roles" => Role::all()->reject(function ($role) {
                 return $role->name === "admin";
             }),
         ]);
     }
 
-    public function addMember(AddMemberRequest $request)
-    {
-        $data = $request->validated();
-
-        User::create([
-            ...$data,
-            "password" => Hash::make(config("env.secret")),
-        ]);
-
-        return back()->with("success", "Data berhasil ditambahkan");
-    }
-
-    public function deleteAllMember()
+    public function deleteAll()
     {
         User::where("role_id", "!=", 1)->delete();
 
