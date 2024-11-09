e92f3568f8c50ed831c93152ad1548262b04af0f
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-10 00:24:19 +0700

    refactor UserService to include flash messages for user actions

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,31 +3,51 @@
 namespace App\Services;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 class UserService
 {
-    public static function store(array $data)
+    private static function flash(string $message)
+    {
+        Session::flash("success", $message);
+    }
+
+    public static function create(array $data)
     {
         User::create([
             ...$data,
             "password" => Hash::make(config("env.secret"))
         ]);
+
+        self::flash("User berhasil ditambahkan");
+    }
+
+    public static function update(User $user, array $data)
+    {
+        $user->update($data);
+
+        self::flash("User berhasil diperbarui");
     }
 
     public static function promote(User $user)
     {
-        $user->update([
-            "role_id" => 2,
-        ]);
+        $user->update(["role_id" => 2]);
+
+        self::flash("User berhasil dipromosikan");
     }
 
     public static function demote(User $user)
     {
-        $user->update([
-            "role_id" => 3,
-        ]);
+        $user->update(["role_id" => 3]);
+
+        self::flash("User berhasil didegradasi");
+    }
+
+    public static function delete(User $user)
+    {
+        $user->delete();
+
+        self::flash("User berhasil dihapus");
     }
 }
