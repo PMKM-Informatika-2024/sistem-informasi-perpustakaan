ca88670d5a5150c7d4cf544ca86f5e0b27a06200
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-10 00:22:44 +0700

    add user creation modal and functionality

---
+++ app/Livewire/Modal/User/Create.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Livewire\Modal\User;
+
+use Livewire\Component;
+use App\Models\Role;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Session;
+use Livewire\Attributes\Validate;
+use Illuminate\Validation\Rule;
+
+class Create extends Component
+{
+    #[Validate(rule: 'required', message: 'Nama harus diisi')]
+    #[Validate(rule: 'unique:users,name', message: 'Nama sudah ada')]
+    public string $name;
+
+    #[Validate(rule: 'required', message: 'Email harus diisi')]
+    #[Validate(rule: 'unique:users,email', message: 'Email sudah ada')]
+    #[Validate(rule: 'email:dns', message: 'Email tidak valid')]
+    public string $email;
+
+    #[Validate(rule: 'required', message: 'Nomor Telepon harus diisi')]
+    #[Validate(rule: 'unique:users,phone_number', message: 'Nomor Telepon sudah ada')]
+    #[Validate(rule: 'numeric', message: 'Nomor Telepon harus angka')]
+    #[Validate(rule: 'phone:ID', message: 'Nomor Telepon tidak valid')]
+    public string $phone_number;
+
+    #[Validate(rule: 'required', message: 'Alamat harus diisi')]
+    #[Validate(rule: 'string', message: 'Alamat harus berupa huruf')]
+    #[Validate(rule: 'max:255', message: 'maksimal 255 karakter')]
+    public string $address;
+
+    #[Validate(rule: 'required', message: 'Password harus diisi')]
+    #[Validate(rule: 'exists:roles,id', message: 'Role tidak valid')]
+    public string $role_id = '';
+
+    public function create()
+    {
+        $this->validate();
+
+        UserService::create($this->all());
+
+        $this->dispatch("close-modal");
+        return $this->redirectRoute("manage user");
+    }
+
+    public function render()
+    {
+        return view('livewire.modal.user.create')->with([
+            "roles" => Role::all()->reject(fn(Role $role) => $role->name === 'admin')
+        ]);
+    }
+}
