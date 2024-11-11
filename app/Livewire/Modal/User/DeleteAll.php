4f2e6b70b0f98f92f051893c3124608e1d4cc573
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-12 01:44:48 +0700

    new component

---
+++ app/Livewire/Modal/User/DeleteAll.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Modal\User;
+
+use Livewire\Component;
+
+class DeleteAll extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.user.delete-all');
+    }
+}
