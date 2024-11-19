14f359282fac1b8592075838dde37bbb40e20950
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-19 19:59:51 +0700

    Add user management features: create, store, edit, and update methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\Rule;
+use App\Http\Requests\User\Create;
+use App\Http\Requests\User\Update;
 
 class UserController
 {
@@ -17,9 +24,43 @@ class UserController
         ]);
     }
 
+    public function create()
+    {
+        return view("dashboard.user.create", [
+            'title' => 'Tambah Member Baru - Manajemen Perpustakaan',
+            'roles' => Role::all()->reject(function ($role) {
+                return $role->name === 'admin';
+            }),
+        ]);
+    }
+
+    public function store(Create $request)
+    {
+        $data = $request->validated();
+
+        User::create([...$data, "password" => Hash::make(config("env.secret"))]);
+        return redirect()->route("manage user")->with("success", "Berhasil Menambahkan Member");
+    }
+
+    public function edit(User $user)
+    {
+        return view("dashboard.user.edit", [
+            "title" => "Edit Member - Manajemen Perpustakaan",
+            "user" => $user
+        ]);
+    }
+
+    public function update(Update $request, User $user)
+    {
+        $data = $request->validated();
+
+        $user->update($data);
+        return redirect()->route("manage user")->with("success", "Berhasil Mengubah Data Member");
+    }
+
     public function deleteAll()
     {
-        User::where('role_id', '!=', 1)->delete();
+        User::excludeAdmin()->delete();
 
         return back()->with('success', 'Semua data berhasil dihapus');
     }
