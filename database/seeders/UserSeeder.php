8789e899b5074a09212b3867b2a5f0247e4e1d6a
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-19 10:27:31 +0700

    update UserSeeder to assign admin role dynamically from Role model

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -12,7 +13,7 @@ class UserSeeder extends Seeder
     {
         User::factory(100)->create();
         User::factory()->create([
-            'role_id' => 1,
+            'role_id' => Role::first('id'),
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make(config('env.secret')),
