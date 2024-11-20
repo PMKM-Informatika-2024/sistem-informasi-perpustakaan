64c55aa585123749a99a62ceba5cba11f2cafc96
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-21 02:58:00 +0700

    add category management functionality with create and update methods, including session handling and success messages

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,14 +3,57 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\Category\Create;
+use App\Http\Requests\Category\Update;
+use Illuminate\Support\Facades\Session;
 
 class CategoryController
 {
     public function index()
     {
+        Session::put('menu', "Manage Kategori");
+
         return view('dashboard.categories', [
-            'title' => 'Manage Kategori',
+            'title' => "Manage Kategori - Manajemen Perpustakaan",
             'categories' => Category::all(),
         ]);
+    }
+
+    public function create()
+    {
+        Session::put('menu', "Tambah Kategori");
+
+        return view("dashboard.category.create", [
+            "title" => "Tambah Kategori - Manajemen Perpustakaan"
+        ]);
+    }
+
+    public function store(Create $request)
+    {
+        $data = $request->validated();
+
+        Category::create($data);
+
+        return redirect()->route("manage category")->with("success", "Berhasil Menambahkan Kategori");
+    }
+
+    public function edit(Category $category)
+    {
+        Session::put('menu', "Edit Kategori");
+
+        return view("dashboard.category.edit", [
+            "title" => "Edit Kategori - Manajemen Perpustakaan",
+            "category" => $category
+        ]);
+    }
+
+    public function update(Update $request, Category $category)
+    {
+        $data = $request->validated();
+
+        $category->update($data);
+
+        return redirect()->route("manage category")->with("success", "Berhasil Memperbarui kategori");
     }
 }
