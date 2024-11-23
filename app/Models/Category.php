59386d1596a9b89525f84921e7a13d91d22411dc
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-23 12:26:15 +0700

    implement soft deletes in Category model; add books relationship; format app.php routing

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasUuids;
+    use HasUuids, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
