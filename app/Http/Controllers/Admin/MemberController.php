0091d83ad0c04680467b65e0504f6cb7c45b90fa
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-26 03:05:27 +0700

    alamak

---
+++ app/Http/Controllers/Admin/MemberController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
+
+class MemberController
+{
+    public function index()
+    {
+        Session::put('menu', 'Manage Member');
+
+        return view('dashboard.admin.members', [
+            'title' => 'Manage Member - Manajemen Perpustakaan',
+        ]);
+    }
+
+    public function deleteAll()
+    {
+        User::excludeAdmin()->delete();
+
+        return back()->with('success', 'Semua member berhasil dihapus');
+    }
+}
