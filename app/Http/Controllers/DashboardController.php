de2e1042d1b347522035e07ea29bd13bb3706eee
rikarani <46773591+rikarani@users.noreply.github.com>
2024-11-30 02:33:11 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Session;
+use App\Models\Book;
+use App\Models\Loan;
+use App\Models\Member;
 
 class DashboardController
 {
     public function index()
     {
-        Session::forget('menu');
-
         return view('dashboard.dashboard', [
             'title' => 'Dashboard - Manajemen Perpustakaan',
+            "members" => Member::all(),
+            "books" => Book::all(),
+            "loans" => Loan::where("status", 1)->get()
         ]);
     }
 }
