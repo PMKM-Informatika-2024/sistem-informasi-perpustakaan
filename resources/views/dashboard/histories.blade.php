0131548b3721627407a010b1b57125d25ac72202
rikarani <46773591+rikarani@users.noreply.github.com>
2024-12-01 01:55:46 +0700

    delete

--- resources/views/dashboard/histories.blade.php
+++
@@ -1,8 +0,0 @@
-<x-dashboard.layout :title="$title">
-  <livewire:karyawan.history.history-table />
-  <div>
-    <x-partial.modal name="create peminjaman">
-      <livewire:modal.history.create />
-    </x-partial.modal>
-  </div>
-</x-dashboard.layout>
