4e6193fd6b7bd59695b9e15513e4df495b48b51b
rikarani <46773591+rikarani@users.noreply.github.com>
2024-12-03 01:15:07 +0700

    Add update and redo modal components to loan management

--- resources/views/dashboard/loans.blade.php
+++ resources/views/dashboard/loans.blade.php
@@ -10,5 +10,12 @@
     <x-partial.modal name="delete">
       <livewire:modal.loan.delete />
     </x-partial.modal>
+    <x-partial.modal name="update peminjaman">
+      <livewire:modal.loan.update />
+    </x-partial.modal>
+    <x-partial.modal name="redo">
+      <livewire:modal.loan.redo />
+    </x-partial.modal>
   </div>
+  <x-partial.toast type="success" />
 </x-dashboard.layout>
